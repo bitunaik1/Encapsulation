5b8a008ebe54cd699826e0346dfba08e7558390f
sachin93-cloud <sachintirkey010@gmail.com>
2025-01-21 21:34:22 +0530

    muin change karli

--- src/encapsulation1/Sample.java
+++ src/encapsulation1/Sample.java
@@ -4,8 +4,9 @@ public class Sample extends Test{
 public static void main(String[] args) {
 	Test t2 = new Test();
 	t2.setId(101);
-	t2.setName("Kiran");
+	t2.setName("Sachin");
 	System.out.println(t2.getId());
+	System.out.println("Hi");
 	System.out.println(t2.getName());	
 }
 }
