54a322bb6b1818b0a3bbcc40e060cbd912790205
DESKTOP-7HO2OQV\Omprakash <bitunaik123@gmail.com>
2025-01-21 22:07:31 +0530

    22:07

--- src/encapsulation1/Sample.java
+++ src/encapsulation1/Sample.java
@@ -7,10 +7,10 @@ public static void main(String[] args) {
 	Test t2 = new Test();
 	t2.setId(101);
 
-	System.out.println("Hi");
+	System.out.println("Hihhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
 	t2.setName("Sachin");
-
-	System.out.println("Hi");
+	System.out.println("Hihhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+	System.out.println("Hihhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
 	System.out.println(t2.getId());
 
    
