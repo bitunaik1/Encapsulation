b1291c4902f4f90fad4e8840a0c174b59a2c6edd
sachin93-cloud <sachintirkey010@gmail.com>
2025-01-21 21:47:21 +0530

    hello

--- src/encapsulation1/Sample.java
+++ src/encapsulation1/Sample.java
@@ -4,7 +4,11 @@ public class Sample extends Test{
 public static void main(String[] args) {
 	Test t2 = new Test();
 	t2.setId(101);
+
+	System.out.println("Hi");
 	t2.setName("Sachin");
+
+	System.out.println("Hi");
 	System.out.println(t2.getId());
 	System.out.println("Hi");
 	System.out.println(t2.getName());	
